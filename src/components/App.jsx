8b04b18224f26764e033a7796e05dabd61899347
alifiarzql <rizqulalifia@gmail.com>
2025-06-03 21:18:25 +0700

    file baru

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Navbar from './Navbar';
+import Footer from './Footer';
 import Login from './Login';
 import Register from './Register';
 import Konsultasi from './Konsultasi';
 import Profil from './Profil';
 import Edukasi from './Edukasi';
+import LupaPassword from './LupaPassword';
+
 import Beranda from '../Beranda';
-import Footer from './Footer';
 
 // Halaman Dummy
 const Layanan = () => <div className="p-4">Halaman Layanan Terdekat</div>;
@@ -15,17 +18,24 @@ const Layanan = () => <div className="p-4">Halaman Layanan Terdekat</div>;
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Beranda />} />
-        <Route path="/konsultasi" element={<Konsultasi />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/layanan" element={<Layanan />} />
-        <Route path="/edukasi" element={<Edukasi />} />
-        <Route path="/profil/:nama" element={<Profil />} />
-      </Routes>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Beranda />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/konsultasi" element={<Konsultasi />} />
+            <Route path="/layanan" element={<Layanan />} />
+            <Route path="/edukasi" element={<Edukasi />} />
+            <Route path="/profil/:nama" element={<Profil />} />
+            <Route path="/lupapassword" element={<LupaPassword />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </Router>
   );
 };
