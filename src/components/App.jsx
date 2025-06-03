112e18af41ece8edacf21678b238ac644aecc0fe
indra <indradwiprabowo27@gmail.com>
2025-06-03 21:42:27 +0700

    beranda

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Footer from './Footer';
 import Login from './Login';
 import Register from './Register';
 import Konsultasi from './Konsultasi';
+import ChatApp from './Chat';
 import Profil from './Profil';
 import Edukasi from './Edukasi';
 import LupaPassword from './LupaPassword';
@@ -26,7 +27,7 @@ const App = () => {
             <Route path="/" element={<Beranda />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/konsultasi" element={<Konsultasi />} />
+            <Route path="/konsultasi" element={<ChatApp />} />
             <Route path="/layanan" element={<Layanan />} />
             <Route path="/edukasi" element={<Edukasi />} />
             <Route path="/profil/:nama" element={<Profil />} />
